5c2fd6bf929a3f4c3fda9f2da1ec12e510c8f921
Matheus Emanuel <matheusemanuel745@gmail.com>
2023-03-16 11:13:29 -0300

    refactor(merge-action): use configuration struct as storage rather state variables

--- contracts/merge-action/MergeNFTStorage.sol
+++ contracts/merge-action/MergeNFTStorage.sol
@@ -2,20 +2,18 @@
 pragma solidity 0.8.18;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IMergeNFT} from "./interfaces/IMergeNFT.sol";
 
 
 /// @title MergeNFT Storage 
 /// @author matheus
 abstract contract MergeNFTStorage {
 
-  /// @notice Address of the origin collection of the NFTs to merge
-  IERC721 public collectionToMerge;
-
   /// @notice TokenId of NFT minted after the merge
   uint256 public tokenId;
 
-  /// @notice Amount of tokens to merge in order to mint new one
-  uint256 public amountToMerge;
+  /// @notice Storage configuration for NFT merge
+  IMergeNFT.Configuration public config;
 
   /// @notice Keeps track of the merged tokens
   mapping(uint256 tokenId => bool merged) public tokensMerged;
