68167102116189bc5390eb9ee4055b0c1de47d21
Matheus Emanuel <matheusemanuel745@gmail.com>
2023-03-31 00:36:59 -0300

    add unlock command skeleton

---
+++ contracts/unlock/Unlock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+
+contract Unlock {
+  //by time and quantity
+}
